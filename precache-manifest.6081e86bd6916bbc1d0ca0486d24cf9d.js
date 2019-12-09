0858cac0ab73fc9b3e6d2a36f0fb8bddef446617
Christofer Flores <cristofer.flort@gmail.com>
2019-12-09 16:08:05 +0000

    Updates

---
+++ precache-manifest.6081e86bd6916bbc1d0ca0486d24cf9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3364235a80144309e40cf03fdcec1922",
+    "url": "/react-legra-site/index.html"
+  },
+  {
+    "revision": "c627661115675b143290",
+    "url": "/react-legra-site/static/css/main.8bbf2446.chunk.css"
+  },
+  {
+    "revision": "4a206626295f7ddbc32e",
+    "url": "/react-legra-site/static/js/2.5df91ffc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-legra-site/static/js/2.5df91ffc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c627661115675b143290",
+    "url": "/react-legra-site/static/js/main.df7140e0.chunk.js"
+  },
+  {
+    "revision": "bee295a500f8742a2f9e",
+    "url": "/react-legra-site/static/js/runtime-main.e3d7ef96.js"
+  }
+]);
